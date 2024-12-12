ca23095da45fbb9ad01286374347002714dcf21d
daniel-beltran <daniel.beltran.248@my.csun.edu>
2024-12-11 19:25:49 -0800

    Added 40.3kHz code to TX for testing

--- TX/TX.ino
+++ TX/TX.ino
@@ -20,6 +20,7 @@ uint16_t Preset_Time_Delay = 100.0;
 uint16_t startTime, endTime,totalTime, flightTime, distance;
 uint16_t GlobalTimeCount = 0;
 
+const long frequency = 40000L;  // Hz
 //===============================================
 //              Function Declarations
 //===============================================
@@ -43,7 +44,14 @@ void enable_timer(); // Enables 1ms Timer
 //              Function Definitions
 //===============================================
 
-
+/*
+ * @brief Initializes all serial port. Creates a 40.3 kHz square wave on pin 9 to be used as transmitter carrier signal.
+ *        Initializes Timer 2. 
+ *
+ * @param 
+ *
+ * @return 
+ */
 void setup() 
 {
   Serial.begin(1200,SERIAL_8N2);            // Initializes the Serial Port Used for serial monitor on computer
@@ -51,38 +59,72 @@ void setup()
   Serial2.begin(1200, SERIAL_8N2);          // Initializes Receiver serial port initiation (Pin 17 on Mega)
   Serial2.setTimeout(500);
 
-  DDRE |= (1<< RED_LED_PIN);
-  DDRE |= (1<< BLUE_LED_PIN);
+   DDRE |= (1<< RED_LED_PIN);
+   DDRE |= (1<< BLUE_LED_PIN);
 
-  // Enable Global interrupts 
+  pinMode(9, OUTPUT);
+  TCCR2A = _BV (WGM20) | _BV (WGM21) | _BV (COM2B1); // fast PWM, clear OC2B on compare
+  TCCR2B = _BV (WGM22) | _BV (CS21);         // fast PWM, prescaler of 8
+  OCR2A =  ((F_CPU / 8) / frequency) - 1;    // zero relative  
+  OCR2B = ((OCR2A + 1) / 2) - 1;             // 50% duty cycle
+  //Enable Global interrupts
   sei();
   timerInit();
   Serial.println("Transmitter is ready");
 }
 
+/*
+ * @brief Infinite Loop Controls Tranmission and receiving.
+ *
+ * @param 
+ *
+ * @return 
+ */
 void loop() {
   user_input();  // Takes in user input the formats it for proper transmition 
   if (key == true){
     enable_timer();
     transmit();
   }  
-  while(Serial2.available() == 0);
-  if(Serial2.available() > 0 ) {
-    receive();
-    disable_timer();
-    GlobalTimeCount = 0; 
-  }
+  // while(Serial2.available() == 0);
+  // if(Serial2.available() > 0 ) {
+  //   receive();
+  //   disable_timer();
+  //   GlobalTimeCount = 0; 
+  // }
   
 }
 
+/*
+ * @brief Enables Timer 1.
+ *
+ * @param 
+ *
+ * @return 
+ */
 void enable_timer() {
   TCCR1B |= 0x01;  // This enables timer or clock source 
 }
+
+/*
+ * @brief Disables Timer 1.
+ *
+ * @param 
+ *
+ * @return 
+ */
 void disable_timer(){
   TCCR1B &= 0xFE; //1111 1110
   TCNT1 = 0;
 }
 
+/*
+ * @brief Initializes and formats Timer 1. Timer is format to a 1 kHz clock speed which triggers an interrupt once every ms.
+ *
+ * @param 
+ *
+ * @return 
+ */
 void timerInit(){
   //Reset Timer1 Control Reg A
   TCCR1A = 0;
@@ -100,6 +142,13 @@ void timerInit(){
   TIMSK1 = (1 << OCIE1A);
 }
 
+/*
+ * @brief Recieves command input from the user. Calls format() after command and Transponder ID are received.
+ *
+ * @param 
+ *
+ * @return 
+ */
 void user_input(){
   //Takes in User Input For Transmittion. 
   Serial.println("Enter Input Command + Transponder ID");
@@ -108,13 +157,26 @@ void user_input(){
   format();
 }
 
+/*
+ * @brief Interrupt Service Routine for Timer 1
+ *
+ * @param GlobalTimeCount - Global Variable is incrimented every time the iterrupt is triggered.
+ *
+ * @return 
+ */
 ISR(TIMER1_COMPA_vect){
   PORTE ^= (1 << BLUE_LED_PIN);
   GlobalTimeCount++;
 }
 
+/*
+ * @brief Formats the input command + transponder id to look like this <U551> inside "data" (U = command; 55 = Ascii value of command; 1 = Slave ID) 
+ *
+ * @param data - global variable that holds the data being sent
+ *        key  - global variable that enables transmission set true
+ * @return 
+ */
 void format(){
-  // Formats the input command + transponder id to look like this <U551> inside "data" (U = command; 55 = Ascii value of command; 1 = Slave ID) 
   data = "";
   data.concat("<");
   int ascii = command.charAt(0);
@@ -126,7 +188,13 @@ void format(){
   key = true;
  }
 
-
+/*
+ * @brief Writes data to Serial port 1 (Pin 18) one character at a time
+ *
+ * @param data - data is cleared
+ *        key  - key is set to false
+ * @return 
+ */
 void transmit()
 {
   for(int i = 0; i < 1; i++){
@@ -139,13 +207,18 @@ void transmit()
     {
       Serial1.write(data.charAt(i));
     }
-    //delay(1000);
   }
-  //Serial.print("DOOOOONE");
   data = "";
   key = false;  
 }
 
+/*
+ * @brief Receives data from Serial Port 2 (Pin 17)  
+ *
+ * @param incomingData - This is where the data which was read is held.
+ *
+ * @return 
+ */
 void receive()
 {
 
@@ -155,18 +228,6 @@ void receive()
   totalTime = GlobalTimeCount; 
   flightTime = (totalTime - 3.0*(Serial_Read_Time) - Preset_Time_Delay)/2.0 ; 
   distance = speedOfSound*flightTime;
-
-  Serial.print("Incoming Data: ");
-  Serial.println(incomingData);
-
-  Serial.print("Flight Time: ");
-  Serial.println(flightTime);
-
-  Serial.print("Total Time: ");
-  Serial.println(totalTime);
-
-  Serial.print("Total Distance: ");
-  Serial.println(distance);
 
   flightTime = 0; 
   distance = 0;
